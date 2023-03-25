38398c11af459723a6b015f186e20423e3699e6d
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2023-03-24 18:46:45 -0600

    Added player icon selection functions?

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const playGamePage = document.querySelector('.play-game-page');
 const popUp = document.querySelector('.pop-up');
+let difficultySetting = 1;
 
 
 
@@ -8,33 +9,6 @@ function showMenu() {
     playGamePage.style.filter = 'blur(2px)';
 }
 
-function selectedDiffButton() {
-    const selectDiffValue = document.querySelector('.selection.option');
-    selectDiffValue.classList.add = 'selected';
-
-    console.log(selectDiffValue);
-
-    console.log(typeof(selectDiffValue))
-
-    console.log(selectDiffValue.value);
-    return selectDiffValue;
-}
-
-function selectedIconButton() {
-
-}
-
-function beginGame() {
-    // creates both players
-    // creates game array
-
-    // listens for users input
-    // Returns choice and adds it to game array
-
-    // Runs computer's choice
-    // Returns choice and adds it to game array
-
-}
 function checkWinningCases () {
     // Checks to see if there are any winning cases
         // Switch cases to identify win cases
@@ -45,24 +19,28 @@ function computerDiffEasy () {
     removeSelectedClass();
     const selectDiffValue = document.querySelector('.selection.option.easy');
     selectDiffValue.classList.add('selected');
+    return difficultySetting = 1;
 }
 
 function computerDiffMedium () {
     removeSelectedClass();
     const selectDiffValue = document.querySelector('.selection.option.medium');
     selectDiffValue.classList.add('selected');
+    return difficultySetting = 2;
 }
 
 function computerDiffHard () {
     removeSelectedClass();
     const selectDiffValue = document.querySelector('.selection.option.hard');
     selectDiffValue.classList.add('selected');
+    return difficultySetting = 3;
 }
 
 function computerDiffImpossible () {
     removeSelectedClass();
     const selectDiffValue = document.querySelector('.selection.option.impossible')
     selectDiffValue.classList.add('selected');
+    return difficultySetting = 4;
 }
 
 function removeSelectedClass () {
@@ -72,6 +50,54 @@ function removeSelectedClass () {
             allSelectionOptions[i].classList.remove('selected');
         }       
     }
+}
+
+function playerSelectedXxxxx () {
+    removeSelectedIconClass();
+    const selectedPlayerIcon = document.querySelector('.x-selection');
+    selectedPlayerIcon.classList.add('selected');
+    const playerIcon = 1;
+    const computerIcon = 0;
+}
+
+function playerSelectedOoooo () {
+    removeSelectedIconClass();
+    const selectedPlayerIcon = document.querySelector('.o-selection');
+    selectedPlayerIcon.classList.add('selected');
+    const playerIcon = 0;
+    const computerIcon = 1;
+}
+
+function removeSelectedIconClass () {
+    const iconSelectionOptions = document.querySelectorAll('.icon.selection');
+    for (i = 0; i < iconSelectionOptions.length; i++) {
+        if (iconSelectionOptions[i].classList.contains('selected')) {
+            iconSelectionOptions[i].classList.remove('selected');
+        }
+    }
+}
+
+
+function beginGame() {
+    
+
+    pageSwitch();
+    // creates both players
+    // creates game array
+    
+    // listens for users input
+    // Returns choice and adds it to game array
+    
+    // Runs computer's choice
+    // Returns choice and adds it to game array
+    
+}
+
+function pageSwitch () {
+    const mainMenu = document.querySelector('.first-page');
+    const gamePage = document.getElementsByClassName('play-game-page');
+    mainMenu.style.display = 'none';
+    gamePage.style.display = 'none';
 }
 
 
