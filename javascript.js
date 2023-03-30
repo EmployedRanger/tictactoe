cdbe716c4f3108fcd522aadb9cde28b9237a45a0
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2023-03-30 01:58:19 -0600

    Added easy and medium functionality

--- javascript.js
+++ javascript.js
@@ -1,10 +1,71 @@
+let difficultySetting = 1;
+let playerIcon = '';
+let computerIcon = '';
+let playerIconArray = '';
 const playGamePage = document.querySelector('.play-game-page');
 const popUp = document.querySelector('.pop-up');
-let difficultySetting = 1;
 
-function showMenu() {
+function applyEvents() {
+    const playAgain = document.querySelector('.try-again');
+    const easy = document.querySelector('.easy');
+    const medium = document.querySelector('.medium');
+    const hard = document.querySelector('.hard');
+    const impossible = document.querySelector('.impossible');
+    const xSelection = document.querySelector('.x-selection');
+    const oSelection = document.querySelector('.o-selection');
+    const beginGame = document.querySelector('.begin-game');
+    const mainMenu = document.querySelector('.main-menu');
+
+    easy.addEventListener('click', () => {
+        computerDiffEasy();
+    });
+
+    medium.addEventListener('click', () => {
+        computerDiffMedium();
+    });
+
+    hard.addEventListener('click', () => {
+        computerDiffHard();
+    });
+
+    impossible.addEventListener('click', () => {
+        computerDiffImpossible();
+    });
+
+    xSelection.addEventListener('click', () => {
+        playerSelectedXxxxx();
+    });
+
+    oSelection.addEventListener('click', () => {
+        playerSelectedOoooo();
+    });
+
+    beginGame.addEventListener('click', () => {
+        PageSwitch();
+    });
+
+    mainMenu.addEventListener('click', () => {
+        reloadPage();
+    });
+
+    playAgain.addEventListener('click', () => {
+        newGame();
+    })
+}
+
+function showMenu(board) {
     popUp.style.display = 'block';
     playGamePage.style.filter = 'blur(2px)';
+    const segmentPieces = document.getElementsByClassName('board-segment');
+    const boardNineSquares = Array.prototype.slice.call(segmentPieces, 13, 22);
+    
+    boardNineSquares.forEach((square, index) => {
+        square.removeEventListener('click', () => {
+            playerTurn(board, square, humanPlayer, computerPlayer);
+        });
+        board.push('');
+    });   
+    return board;
 }
 
 function computerDiffEasy () {
@@ -44,9 +105,14 @@ function removeSelectedClass () {
     }
 }
 
-let playerIcon = '';
-let computerIcon = '';
-let playerIconArray = '';
+function RemoveSelectedIconClass () {
+    const iconSelectionOptions = document.querySelectorAll('.icon.selection');
+    for (i = 0; i < iconSelectionOptions.length; i++) {
+        if (iconSelectionOptions[i].classList.contains('selected')) {
+            iconSelectionOptions[i].classList.remove('selected');
+        }
+    }
+}
 
 function playerSelectedXxxxx () {
     RemoveSelectedIconClass();
@@ -64,8 +130,10 @@ function playerSelectedXxxxx () {
     playerIcon = 'X';
     computerIcon = 'O';
     
-    playerIconArray = [playerIcon, computerIcon];
-    return playerIconArray;
+    return [
+        playerIcon,
+        computerIcon
+    ]
 
 }
 
@@ -85,18 +153,10 @@ function playerSelectedOoooo () {
 
     playerIcon = 'O';
     computerIcon = 'X';
-    playerIconArray = [playerIcon, computerIcon];
-    return playerIconArray;
-
-}
-
-function RemoveSelectedIconClass () {
-    const iconSelectionOptions = document.querySelectorAll('.icon.selection');
-    for (i = 0; i < iconSelectionOptions.length; i++) {
-        if (iconSelectionOptions[i].classList.contains('selected')) {
-            iconSelectionOptions[i].classList.remove('selected');
-        }
-    }
+    return [
+        playerIcon,
+        computerIcon
+    ]
 }
 
 function PageSwitch () {
@@ -104,238 +164,158 @@ function PageSwitch () {
     const gamePage = document.getElementsByClassName('play-game-page');
     mainMenu.style.display = 'none';
     gamePage[1].style.display = 'grid';
-    // ScreenController(playerIcon, computerIcon);
-    GetBoardElements(playerIconArray);
+
+    pregameSetup(playerIcon, computerIcon, difficultySetting);
 }
 
-let currentPlayerIndex = 0;
+function pregameSetup(playerIcon, computerIcon, difficultySetting) {
+    const humanPlayer = Player('Player', playerIcon);
+    const computerPlayer = Player('Computer', computerIcon);
+    const board = [];
+    // if (humanPlayer.icon === 'X') {
+    //     const currentPlayerIndex = 0
+    // }
+    getBoardElements(board, humanPlayer, computerPlayer);
+    // console.log(board);
+    gamePlay(board, humanPlayer, computerPlayer, difficultySetting);
+}
 
-function GetBoardElements(playerIconArray) {
-    // const one = document.querySelector('.one');
-    // const two = document.querySelector('.two');
-    // const three = document.querySelector('.three');
-    // const four = document.querySelector('.four');
-    // const five = document.querySelector('.five');
-    // const six = document.querySelector('.six');
-    // const seven = document.querySelector('.seven');
-    // const eight = document.querySelector('.eight');
-    // const nine = document.querySelector('.nine');
+function gamePlay(board, humanPlayer, computerPlayer, difficultySetting) {
+    if (computerPlayer.icon === 'X') {
+        computerTurn(board, difficultySetting, computerPlayer);
+    }
+}
+
+function newGame(board) {
+    console.log('board is: ', board);
+
+    clearBoard(board);
+    pregameSetup(playerIcon, computerIcon, difficultySetting);
+}
+
+function clearBoard(board) {
+    console.log('board is: ', board);
+    const allSquares = Array.from(document.querySelector('.part'));
+    allSquares.forEach(element => {
+        element.innerHTML = '';
+    });
+    for (let i = 0; i < 9; i++) {
+        board[i] = '';
+    }
+    console.log(board);
+}
+
+function getBoardElements(board, humanPlayer, computerPlayer) {
     const segmentPieces = document.getElementsByClassName('board-segment');
     const boardNineSquares = Array.prototype.slice.call(segmentPieces, 13, 22);
-    // const boardNineSquares = document.getElementsByClassName('part');
-    const board = [];
     
     boardNineSquares.forEach((square, index) => {
         square.classList.add('part');
-        // console.log(square.className);
         square.addEventListener('click', () => {
-            const currentPlayerIcon = playerIconArray[currentPlayerIndex]; 
-            if (board[index] === '') {
-                square.innerHTML = currentPlayerIcon;
-                board[index] = currentPlayerIcon;
-                if(currentPlayerIcon === 'X') {
-                    square.classList.add('x-color');
-                } else {
-                    square.classList.add('o-color');
-                }
-                // console.log(currentPlayerIcon);
-                // console.log(board);
-                currentPlayerIndex = (currentPlayerIndex + 1) % playerIconArray.length;
-                // computerTurn(playerIconArray, currentPlayerIcon, board);
-                playGame(board, currentPlayerIcon);
-            }
-            // return currentPlayerIcon();
+            playerTurn(board, square, humanPlayer, computerPlayer);
         });
         board.push('');
-    });    
-    // playGame(currentPlayerIcon);
+    });   
+    return board;
 }
 
-function playGame(board, currentPlayerIcon) {
-    const turnCounter = 0;
-
-}
-
-function computerTurn (playerIconArray, currentPlayerIcon, board) {
-    // const emptyBoxes = document.getElementsByClassName
-    const emptySquares = Array.from(document.getElementsByClassName('part'));
-    // const emptySquares = Array.prototype.slice.call(allBoardSegments, 13, 22);
-    console.log(emptySquares);
-
-    if(emptySquares.length > 0) {
-        const randomIndex = Math.floor(Math.random() * emptySquares.length);
-        const randomSquare = emptySquares[randomIndex];
-        const computerIcon = playerIconArray.filter(icon => icon !== currentPlayerIcon[0]);
-        randomSquare.innerHTML = computerIcon;
-        randomSquare.classList.add(computerIcon === 'X' ? 'x-color' : 'o-color');
-        board[parseInt(randomSquare.dateset.index)] = computerIcon;
-        console.log(board);
-        currentPlayerIndex = (currentPlayerIndex + 1) % playerIconArray.length;
-    }
-}
-
-function easyDifficulty (board, turnCounter) {
-    const computerChoice = turnCounter;
-    if (board[computerChoice] === '') {
-        board[computerChoice].innerHTML = computerIcon;
-    }
-    
-    turnCounter++;
-    console.log('turn:', turnCounter);
-    return computerChoice;
-}
-
-function mediumDifficulty () {
-    if (currentPlayerIcon === 'X') {
-        console.log(board)
-        const randomIndex = Math.floor(Math.random() * 9);
-        console.log('randomIndex is', randomIndex);
-        const randomSquare = board[randomIndex];
-        console.log('randomSquare is: ', randomSquare);
-        const computerIcon = 'O';
-
-        if (randomSquare === '') {
-            board[randomIndex].innerHTML = computerIcon;
-            console.log('success')
+function playerTurn(board, square, humanPlayer, computerPlayer) {
+    const squareIndex = square.id - 1;
+    if (square.innerHTML === '') {
+        if(humanPlayer.icon === 'X') {
+            board[squareIndex] = 'X';
+            square.classList.add('x-color');
+            square.innerHTML = 'X';
         } else {
-            playGame()
+            board[squareIndex] = 'O';
+            square.classList.add('o-color');
+            square.innerHTML = 'O';
         }
-    } 
+        const result = checkWinningCases(board, humanPlayer);
+        computerTurn(board, difficultySetting, computerPlayer);
+        // update(board);
+    } else {
+        console.log('square was not blank');
+        console.log(board);
+        return;
+    }
 }
 
+function checkWinningCases(board, player) {
+    const winnerMessage = document.querySelector('.winner.is');
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+    for (let i = 0; i < winningCombinations.length; i++) {
+        console.log(board)
+        const [a, b, c] = winningCombinations[i];
+        if (player.icon === board[a] && board[a] === board[b] && board[b] === board[c]) {
+            winnerMessage.innerHTML = player.name;
+            showMenu(board);
+        }
+    }
+    return;
+}
 
+function resetGame() {
+    for (let i = 0; i < 9; i++) {
+        board[i] = '';
+        document.getElementById(i).innerHTML = '';
+    }
+}
 
+function computerTurn(board, difficultySetting, computerPlayer) {
+    if (difficultySetting === 1) {
+        easyDifficulty(board, computerPlayer);
+    } else if(difficultySetting === 2) {
+        mediumDifficulty(board, computerPlayer);
+    }
+    const result = checkWinningCases(board, computerPlayer);
+}
 
+function easyDifficulty (board, computerPlayer) {
+    for (let i = 0; i < board.length; i++) {
+        if (board[i] === '') {
+            board[i] = computerPlayer.icon;
+            const square = document.getElementsByClassName('board-segment')[i + 13];
+            setTimeout(() => {
+                square.innerHTML = computerPlayer.icon;
+                square.classList.add(computerPlayer.icon === 'X' ? 'x-color' : 'o-color');
+            }, 100);
+            // update(board);
+            return board;
+        }
+    }
+    return board;
+}
 
+function mediumDifficulty(board, computerPlayer) {
+    let index = Math.floor(Math.random() * 9);
+    console.log('index is:', index);
+    console.log('boardIndex:', board[index])
+    if (board[index] === '') {
+      board[index] = computerPlayer.icon;
+      document.getElementById(index).innerHTML = computerPlayer.icon;
+    } else {
+      mediumDifficulty(board, computerPlayer);
+    }
+}
 
-// const Player = (name, icon) => {
-//     const getName = () => name;
-//     const getIcon = () => icon;
-
-//     return {
-//         getName,
-//         getIcon
-//     };
-// }
-
-// function Gameboard () {
-//     const rows = 3;
-//     const columns = 3;
-//     const board = [];
-
-//     for (let i = 0; i < rows; i++) {
-//         board[i] = [];
-//         for(let j = 0; j < columns; j++) {
-//             board[i].push(BoardSegment());
-//         }
-//     }
-
-//     const getBoard = () => board;
-
-//     const selectSquare = (column, player) => {
-//         const availableSegments = board.filter((row) => 
-//         row[column].getValue() === '').map(row => row[column]);
-
-//         if (!availableSegments.length) return;
-
-//         // More stuff here
-//     };
-
-//     const printBoard = () => {
-//         const boardWithSegmentValues = board.map((row) => 
-//         row.map((segment) => segment.getValue()));
-//         console.log(boardWithSegmentValues);
-//     };
-
-//     return { getBoard, selectSquare, printBoard };
-// }
-
-// function BoardSegment() {
-//     let value = '';
-
-//     const addIcon = (player) => {
-//         value = player;
-//     };
-
-//     const getValue = () => value;
-
-//     return {
-//         addIcon,
-//         getValue
-//     };
-// }
-
-// function GameController(
-//     playerIcon,
-//     computerIcon,
-//     humanPlayerName = 'Player',
-//     computerPlayerName = 'Computer'
-// ) {
-//     const humanPlayer = Player(humanPlayerName, playerIcon);
-//     console.log('human player is: ', humanPlayer.getIcon);
-//     const computerPlayer = Player(computerPlayerName, computerIcon);
-//     const board = Gameboard();
-//     return {
-//         humanPlayer,
-//         computerPlayer,
-//         board
-//     };
-// }
-
-// function ScreenController(playerIcon, computerIcon) {
-//     const game = GameController(playerIcon, computerIcon);
-//     const boardDiv = document.querySelector('.game-board');
-    
-//     RenderBoard(boardDiv, game.board.getBoard());
-
-//     const updateScreen = () => {
-//         // clears all board segments
-//         boardDiv.textContent = '';
-
-//         // Gets newest version of game-board
-//         const board = game.getBoard();
-
-//         const BoardSegments = boardDiv.querySelectorAll('.board-segment');
-//         BoardSegments.forEach((segment, index) => {
-//             const row = Math.floor(index / 3);
-//             const column = index % 3;
-//             const value = board[row][column].getValue();
-//             segment.textContent = value;
-//         });
-//     }    
-// }
-
-// function RenderBoard(boardDiv, gameBoard) {
-//     // const boardDiv = document.querySelector('.game-board');
-
-//     for(let i = 0; i < 3; i++) {
-//         const rowDiv = document.createElement('div');
-//         rowDiv.classList.add('row');
-//         for(let j = 0; j < 3; j++) {
-//             const segmentDiv = document.createElement('div');
-//             segmentDiv.classList.add('board-segment');
-//             segmentDiv.setAttribute('data-row', i);
-//             segmentDiv.setAttribute('data-column', j);
-//             rowDiv.appendChild(segmentDiv);
-//         }
-//         boardDiv.appendChild(rowDiv);
-//     }
-//     console.log(boardDiv)
-// }
-
-// function checkWinningCases () {
-//     // Checks to see if there are any winning cases
-//         // Switch cases to identify win cases
-    
-// }
-
-// factory function to create both players
+const Player = (name, icon) => {
+    return {
+        name,
+        icon
+    };
+}
 
 function reloadPage () {
     location.reload();
 }
 
 window.onload = (event) => {
+    applyEvents();
     computerDiffEasy();
     playerSelectedXxxxx();
 }
